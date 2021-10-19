50dbda9a53c6f25aba984a4b1765f557361d92a3
NerisD <dimitri@smith.re>
2021-10-19 10:29:55 +0300

    Initial Commit

---
+++ Lab10_Calculator_Part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab10_Calculator_Part1
+//
+//  Created by Dimitri SMITH on 19/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
